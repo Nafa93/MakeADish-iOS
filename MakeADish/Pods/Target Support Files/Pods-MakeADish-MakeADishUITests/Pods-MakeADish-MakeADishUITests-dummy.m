7c558b9a9575d9b97388e7db9527e8cc250b912f
Nicolas Amorosino <namorosino@deviget.com>
2020-04-19 01:00:31 -0300

    Added swiftlint although it isn't working

---
+++ MakeADish/Pods/Target Support Files/Pods-MakeADish-MakeADishUITests/Pods-MakeADish-MakeADishUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MakeADish_MakeADishUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MakeADish_MakeADishUITests
+@end
---
+++ MakeADish/Pods/Target Support Files/Pods-MakeADishTests/Pods-MakeADishTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MakeADishTests : NSObject
+@end
+@implementation PodsDummy_Pods_MakeADishTests
+@end
